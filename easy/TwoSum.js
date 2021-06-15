8e9e604ca9e4d539df91a39312341b37539304cf
Nguyen Tien Dat <tiendat@redweb.dk>
2021-06-15 23:01:55 +0700

    update solution

--- easy/TwoSum.js
+++ easy/TwoSum.js
@@ -50,15 +50,18 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-  for(let i = 0; i < nums.length - 1;i++){
-    for(let j = i + 1; j < nums.length; j++){
-      if(nums[i] + nums[j] == target){
-        return [i, j];
-      }
-    }
-  }
+var twoSum = function (nums, target) {
+  if (nums.length === 2)
+    return nums[0] + nums[1] === target ? [0, 1] : null;
 
-  return "Could not find such a pair";
+  let result = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    const partner = target - nums[i];
+
+    const resultPartner = nums.findIndex((el, idx) => el === partner && idx !== i);
+
+    if (resultPartner !== -1) return [i, resultPartner];
+  }
 };
 //leetcode submit region end(Prohibit modification and deletion)
